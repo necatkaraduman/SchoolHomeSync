9890a9557d9098cda7225bf7d4acfbf77267becd
WilliamM2020 <moolman.william@gmail.com>
2018-09-27 11:54:57 +0100

    New stuff

--- JavaSQLiteConnection/src/javasqliteconnection/JavaSQLiteConnection.java
+++ JavaSQLiteConnection/src/javasqliteconnection/JavaSQLiteConnection.java
@@ -15,7 +15,8 @@ public class JavaSQLiteConnection {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Sqlite conn = new Sqlite("ExtraLessons.sqlite");
+        conn.connect();
     }
     
 }
