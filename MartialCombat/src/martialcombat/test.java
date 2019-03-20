36d53b7229b5f023c2e076610b7c96f5723f237b
WilliamMoolman <moolman.william@gmail.com>
2019-03-20 16:53:54 +0200

    MC Finished

--- MartialCombat/src/martialcombat/test.java
+++ MartialCombat/src/martialcombat/test.java
@@ -11,12 +11,7 @@ package martialcombat;
  */
 public class test {
     public static void main(String[] args) {
-        Monster m1 = new Monster();
-        Monster m2 = new Monster();
-        TrainingDummy td1 = new TrainingDummy();
         Player p1 = new Player();
-        System.out.println("");
-        p1.setTarget(m1);
-        p1.playerAttack();
+        System.out.println(p1.toString());
     }
 }
